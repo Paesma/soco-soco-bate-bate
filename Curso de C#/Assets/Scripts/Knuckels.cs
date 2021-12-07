c5352a0ce0c1c42829028062062279a7dd0e5cb1
Estevão Alves <pomerwe@gmail.com>
2021-12-07 00:52:24 -0300

    Acelerando

--- Curso de C#/Assets/Scripts/Knuckels.cs
+++ Curso de C#/Assets/Scripts/Knuckels.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,9 @@ using UnityEngine;
 public class Knuckels : MonoBehaviour
 {
 
-    public float Velocidade;
+    public float velocidadeMax = 20;
+    public float multiplicadorAnalogico = 0.43f;
+    public float aceleracao = 1.4f;
     private Animator animator;
     private Rigidbody2D player;
     private SpriteRenderer sprite;
@@ -15,12 +18,17 @@ public class Knuckels : MonoBehaviour
     bool EstaPulando;
     public float ForcaDoPulo;
     public float AnguloDeContato;
-    bool estaSocando;
+    bool estaSocando = false;
     bool animacao;
 
+    public float velocidadeAtual = 0;
+    public float animationSpeed = 0.1f;
+    public float animationMultiplicator = 0.1f;
+    public float minAnimationSpeed = 0.5f;
+    private float baseAnimationSpeed = 1f;
 
-
-    
+    private bool isMaxSpeed = false;
+    public float maxSpeedAnimationMultiplicator = 2.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +36,7 @@ public class Knuckels : MonoBehaviour
         animator = GetComponent<Animator>();
         player = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-        
+
     }
 
     // Update is called once per frame
@@ -39,7 +47,26 @@ public class Knuckels : MonoBehaviour
         //!estasocando - estasocando == false
         if (!estaSocando)
         {
-            player.velocity = new Vector2(hori * Velocidade, player.velocity.y);
+            Vector2 velocidadeAtual = player.velocity;
+
+            bool andandoPraFrente = !sprite.flipX;
+
+            if (
+                (velocidadeAtual.x < velocidadeMax && andandoPraFrente) ||
+                (velocidadeAtual.x > -velocidadeMax && !andandoPraFrente)
+                )
+            {
+                float velocidade = (hori * multiplicadorAnalogico) * (aceleracao * aceleracao);
+
+                velocidadeAtual.x += velocidade;
+                velocidadeAtual.x = (float)Math.Round(velocidadeAtual.x, 2);
+
+                player.velocity = velocidadeAtual;
+
+                this.velocidadeAtual = velocidadeAtual.x;
+            }
+
+            isMaxSpeed = Math.Abs(velocidadeAtual.x) > velocidadeMax - 3;
 
             if (hori > 0 || hori < 0)
             {
@@ -59,7 +86,28 @@ public class Knuckels : MonoBehaviour
                 sprite.flipX = true;
             }
         }
-       
+
+        if (animator.GetBool("andando"))
+        {
+            var newSpeedAnimation = (float)Math.Abs(Math.Round(player.velocity.x)) * animationMultiplicator;
+
+            if (isMaxSpeed)
+                newSpeedAnimation *= maxSpeedAnimationMultiplicator;
+
+            if (newSpeedAnimation < minAnimationSpeed)
+            {
+                newSpeedAnimation = minAnimationSpeed;
+            }
+
+            animator.speed = newSpeedAnimation;
+        }
+        if (estaSocando)
+        {
+            animator.speed = baseAnimationSpeed;
+        }
+
+        animationSpeed = animator.speed;
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             animator.SetTrigger("Attack1");
@@ -68,7 +116,7 @@ public class Knuckels : MonoBehaviour
         }
 
         EstaNoChao = Physics2D.OverlapCircle(ChecadorDeSolo.position, AnguloDeContato, Solo);
-        if(EstaNoChao)
+        if (EstaNoChao)
         {
             animacao = true;
         }
@@ -76,7 +124,24 @@ public class Knuckels : MonoBehaviour
         {
             animacao = false;
         }
-        if(animacao)
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetTrigger("Attack1");
+            estaSocando = true;
+            player.velocity = Vector2.zero;
+        }
+
+        EstaNoChao = Physics2D.OverlapCircle(ChecadorDeSolo.position, AnguloDeContato, Solo);
+        if (EstaNoChao)
+        {
+            animacao = true;
+        }
+        else
+        {
+            animacao = false;
+        }
+        if (animacao)
         {
             animator.SetBool("pulo", false);
         }
@@ -85,16 +150,17 @@ public class Knuckels : MonoBehaviour
             animator.SetBool("pulo", true);
             animator.SetBool("andando", false);
         }
-        if(Input.GetKeyDown(KeyCode.Space) && EstaNoChao)
+        if (Input.GetKeyDown(KeyCode.Space) && EstaNoChao)
         {
             EstaPulando = true;
         }
-        if(EstaPulando)
+        if (EstaPulando)
         {
             player.AddForce(new Vector2(0, ForcaDoPulo));
             EstaPulando = false;
         }
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
