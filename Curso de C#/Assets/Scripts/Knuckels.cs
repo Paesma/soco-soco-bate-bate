b5d4bbc1c930a041a1b5c3bf42ca00c4b478a8b7
Michael <hyuzaky@gmail.com>
2021-12-07 00:29:33 -0300

    Pulo pronto

--- Curso de C#/Assets/Scripts/Knuckels.cs
+++ Curso de C#/Assets/Scripts/Knuckels.cs
@@ -9,8 +9,18 @@ public class Knuckels : MonoBehaviour
     private Animator animator;
     private Rigidbody2D player;
     private SpriteRenderer sprite;
+    public LayerMask Solo;
+    public Transform ChecadorDeSolo;
+    bool EstaNoChao;
+    bool EstaPulando;
+    public float ForcaDoPulo;
+    public float AnguloDeContato;
+    bool estaSocando;
+    bool animacao;
 
-    bool estaSocando = false;
+
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +28,7 @@ public class Knuckels : MonoBehaviour
         animator = GetComponent<Animator>();
         player = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
@@ -29,31 +40,65 @@ public class Knuckels : MonoBehaviour
         if (!estaSocando)
         {
             player.velocity = new Vector2(hori * Velocidade, player.velocity.y);
-        }
 
-        if (hori > 0)
-        {
-            sprite.flipX = false;
-        }
-        else if (hori < 0)
-        {
-            sprite.flipX = true;
-        }
+            if (hori > 0 || hori < 0)
+            {
+                animator.SetBool("andando", true);
+            }
+            else
+            {
+                animator.SetBool("andando", false);
+            }
 
-        if (hori > 0 || hori < 0)
-        {
-            animator.SetBool("andando", true);
+            if (hori > 0)
+            {
+                sprite.flipX = false;
+            }
+            else if (hori < 0)
+            {
+                sprite.flipX = true;
+            }
         }
-        else
-        {
-            animator.SetBool("andando", false);
-        }
+       
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             animator.SetTrigger("Attack1");
             estaSocando = true;
             player.velocity = Vector2.zero;
         }
+
+        EstaNoChao = Physics2D.OverlapCircle(ChecadorDeSolo.position, AnguloDeContato, Solo);
+        if(EstaNoChao)
+        {
+            animacao = true;
+        }
+        else
+        {
+            animacao = false;
+        }
+        if(animacao)
+        {
+            animator.SetBool("pulo", false);
+        }
+        else
+        {
+            animator.SetBool("pulo", true);
+            animator.SetBool("andando", false);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && EstaNoChao)
+        {
+            EstaPulando = true;
+        }
+        if(EstaPulando)
+        {
+            player.AddForce(new Vector2(0, ForcaDoPulo));
+            EstaPulando = false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(ChecadorDeSolo.transform.position, AnguloDeContato);
     }
     public void FimDoSoco()
     {
